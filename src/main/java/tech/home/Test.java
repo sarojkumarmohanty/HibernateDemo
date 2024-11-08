d86347f867b5d8cfcff1cdf4fc5c7721fea6ba4e
Saroj <sarojkumarmohanty@users.noreply.github.com>
2024-11-08 17:07:47 +0530

    Update Test.java adding some demo lines

--- src/main/java/tech/home/Test.java
+++ src/main/java/tech/home/Test.java
@@ -17,6 +17,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		//select * from emp_demo;
+		//adding some demo lines
 		final String seQuery="SELECT employeeId,lastName FROM EmpDemo WHERE departmentId=?1 and salary > ?2";
 		
 		Configuration configuration = new Configuration();
